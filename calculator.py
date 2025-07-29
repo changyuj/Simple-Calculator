cbffd5c0bc854ce4b0bcba6659badfdbc3366313
Yu Ju Chang <yuju.chang86@gmail.com>
2025-07-29 16:58:22 -0500

    modified code to different way of calculating

--- calculator.py
+++ calculator.py
@@ -1,31 +1,29 @@
-# --- Arithmetic Functions ---
+# --- evaluate the input ---
 
-def add(x,y):
-    """return the sum of two numbers."""
-    return x + y
+def equals(prompt):
+    """evaluate the string"""
+    
+    try :
+        total = str(eval(prompt))
+    
+    #catch division by 0 error
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero!"      
+    # catch syntax error
+    except SyntaxError:
+        return "Syntax error!" 
 
-def subtract(x,y):
-    """return the difference of two numbers."""
-    return x - y
+    return total
 
-def multiply(x,y):
-    """return the product of two numbers."""
-    return x * y
 
-def divide(x,y):
-    """return the division of two numbers."""
-    return x / y
+# --- number validation ---
 
-# --- User Interaction Functions ---
-
-def get_input(prompt):
-    """Get input and validate"""
-    while True:
-        try:
-            value = float(input(prompt))
-            return value
-        except ValueError:
-            print("Invalid input, please enter a number.")
+def check_input(prompt):
+    """check input and validate"""
+    for char in prompt:
+        if char.isalpha():
+            return True
+    return False
 
 # --- Main Calculator Logic ---
 
@@ -34,41 +32,26 @@ def main():
     print("\n=== Simple Command-Line Calculator ===")
     
     while True:
-        
-        number1 = get_input("Enter the first number: ")
-        
-        print("\nSelect operations:")
-        print("1. Add (+)")
-        print("2. Subtract (-)")
-        print("3. Multiply (*)")
-        print("4. Divide (/)")
-        print("5. Exit")
-        
-        choice = input("Enter choice(1/2/3/4/5): ")
-        
-        if choice == '5':
-            print("Exiting calculator. Goodbye!")
-            break
-        
-        if choice in ('1', '2', '3', '4'):
-            number2 = get_input("Enter the second number: ")
-            
-            if choice == '1':
-                print(f"{number1} + {number2} = {add(number1, number2):.2f}")
-            elif choice == '2':
-                print(f"{number1} + {number2} = {subtract(number1, number2):.2f}")
-            elif choice == '3':
-                print(f"{number1} + {number2} = {multiply(number1, number2):.2f}")
-                #handle division by 0
-            elif choice == '4':
-                if number2 == 0:
-                    print("Error: cannot divide by zero!")
-                else:
-                    print(f"{number1} + {number2} = {divide(number1, number2):.2f}")  
-        else:
-            print("Invalid input. Please enter a valid number.")
-            
-        print("\n------------------------------------------------") #separator line 
+                
+       print("\nWhat would you like to do?")
+       print("1. Enter math problem")
+       print("2. Exit")
+       
+       choice = input("Enter choice (1/2): ")
+       if choice == '2':
+           print("Exiting calculator. Goodbye!")
+           break
+       elif choice == '1':   
+            print("\nEnter your math question: ")
+            math_problem = input("\n(ie. 1+1 or 2-1 or 2/1 or 1*2): ")
+            if check_input(math_problem):
+                print("Invalid input. Please don't include characters.")
+            else:
+                result = equals(math_problem)
+                print(f"{math_problem} = {result}")
+                print("\n------------------------------------------------") #separator line 
+       else:
+            print("invalid choice. ")
 
 if __name__ == "__main__":
     main()
